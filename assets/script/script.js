6ba980c21c41eac67e8c30432ec600f9940cf5be
SyllasBraga <syllasbraga2@gmail.com>
2022-06-25 19:36:22 -0300

    Version one

---
+++ assets/script/script.js
@@ -0,0 +1,67 @@
+const cards = document.querySelectorAll('.card');
+let hasFlipedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+    if (lockBoard) {
+        return;
+    }
+    if (this === firstCard) {
+        return;
+    }
+    
+    this.classList.add('flip');
+    if (!hasFlipedCard) {
+        hasFlipedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlipedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCard();
+        return;
+    }
+    unflipCard();
+}
+
+function disableCard() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetBoard();
+}
+
+function unflipCard() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlipedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function embaralhar() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    });
+})();
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+});
+
+
+
+
+
